cd2de56fcd75ea69b9c136d3e9eb2844996bb6bc
fengzhengyi <512369179@qq.com>
2022-04-17 14:50:06 +0800

    adding a Second model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root "articles#index"
   # Defines the root path route ("/")
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_13_151653) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_17_052219) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_151653) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
